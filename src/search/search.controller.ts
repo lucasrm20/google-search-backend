d7be05c119147253a8cb5c0c3bdf036547bb722f
Lucas Rodrigues Moura <lucas.rodrigues20@outlook.com>
2023-09-04 22:41:15 -0300

    enabel cors

--- src/search/search.controller.ts
+++ src/search/search.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, ParseIntPipe, Query } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { SearchResponse } from './interfaces/search-response.interface';
 import { SearchService } from './search.service';
 
@@ -12,7 +12,7 @@ export class SearchController {
   @Get()
   search(
     @Query('search') searchTerm: string,
-    @Query('offset', ParseIntPipe) offset: number
+    @Query('offset') offset?: number
   ): Promise<SearchResponse[]> {
     return this.searchService.search(searchTerm, offset);
   }
